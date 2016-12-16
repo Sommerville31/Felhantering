4486f90aad5821e4b17905e6a8c84118a3b79839
Sommerville31 <sebastiansommerville3@gmail.com>
2016-12-16 14:52:11 +0100

    Övn1+

--- Felhantering/Program.cs
+++ Felhantering/Program.cs
@@ -11,6 +11,14 @@ namespace Felhantering
         static void Main(string[] args)
         {
             //Övning1
+            Console.Write("Ange din inkomst: ");
+            int inkomst = int.Parse(Console.ReadLine());
+            Console.Write("Ange antal timmar: ");
+            int timmar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h");
+
+            Console.ReadKey();
         }
     }
 }
